3ec2b211b47ff5fc02af030987732eb27669b33a
Derrode Walter Cheale <144121696+deeosy@users.noreply.github.com>
2025-04-01 13:01:39 +0100

    added animation to the homepage cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 //pages/Home.jsx
-
-import React from 'react'
+import React, { useEffect } from 'react'
 import nextArrow from '../icons/right-arrow.png'
 import SliderHeroSection from '../components/SliderHeroSection'
 import wardrobe from '../icons/wardrobe-outline.png'
@@ -8,9 +7,21 @@ import brain from '../icons/brain-outline.png'
 import camera from '../icons/camera-outline.png'
 import SmartFeaturesCard from '../components/SmartFeaturesCard'
 import { Link } from 'react-router-dom'
-
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 export default function Home() {
+    useEffect(() => { // Animation for Smart Feature Cards
+        Aos.init({
+          duration: 1000, 
+          once: false, 
+          mirror: true,
+          offset: 200,
+          disable: 'mobile',
+          anchorPlacement: 'top-bottom',       
+        });
+      }, []);
+
   return (
     <div className="text-[#212529] manrope ">
         <div className='w-full bg-[#E5E5E5]  ' >
@@ -35,19 +46,15 @@ export default function Home() {
             <p className='text-[14px] py-[20px] lg:py-[40px] md:text-[20px] xl:text-[24px] text-center'>Experience the future of wardrobe management with our innovative features</p>
             <div className="flex flex-col gap-4 lg:flex-row lg:justify-between lg:gap-8 lg:py-[50px] lg:px-10 lg:mx-auto max-w-[1300px] ">
                 <div className="flex flex-col gap-4 sm:flex-row lg:gap-8">
-                    <SmartFeaturesCard icon={wardrobe} title='Wardrobe Organization' descprition='Categorize and manage your wardrobe efficiently' />
-                    <SmartFeaturesCard icon={brain} title='AI Styling' descprition='Get personalized outfit recommendations powered by AI' />
-
+                    <SmartFeaturesCard delay={0} icon={wardrobe} title='Wardrobe Organization' description='Categorize and manage your wardrobe efficiently' />
+                    <SmartFeaturesCard delay={200} icon={brain} title='AI Styling' description='Get personalized outfit recommendations powered by AI' />
                 </div>
                 <div className="flex flex-col gap-4 sm:flex-row lg:gap-8">
-                    <SmartFeaturesCard icon={camera} title='Visual Catalog' descprition='Keep a visual record of all your clothing items' />
-                    <SmartFeaturesCard icon={wardrobe} title='Wardrobe Organization' descprition='Categorize and manage your wardrobe efficiently' />
-
+                    <SmartFeaturesCard delay={400} icon={camera} title='Visual Catalog' description='Keep a visual record of all your clothing items' />
+                    <SmartFeaturesCard delay={600} icon={wardrobe} title='Wardrobe Organization' description='Categorize and manage your wardrobe efficiently' />
                 </div>
-
             </div>
         </div>
-
     </div>
   )
 }
